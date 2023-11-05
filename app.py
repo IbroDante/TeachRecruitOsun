425e08fc93745bb3b4345f3c28d6a18a072f3363
ibrodante <ibroman02@yahoo.com>
2023-11-05 11:18:20 +0100

    osun recruit commit

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
-from flask_wtf.file import FileField
+from flask_wtf.file import FileField, FileAllowed
 from flask import send_file
 import os
 from werkzeug.utils import secure_filename
@@ -19,7 +19,7 @@ from wtforms.widgets import TextArea
 
 app = Flask(__name__)
 # app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root123@localhost/edung'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root123@localhost/teachosun'
 app.secret_key = 'root1234'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -57,14 +57,20 @@ class User(db.Model, UserMixin):
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    photo_path = db.Column(db.String(255), nullable=True)
     name = db.Column(db.String(100), nullable=False)
     birthday = db.Column(db.DateTime, nullable=True)
     gender = db.Column(db.String(10), nullable=False)
+    marital = db.Column(db.String(10), nullable=False)
     phonenumber = db.Column(db.String(20), nullable=False)
-    selectcourse = db.Column(db.String(20), nullable=False)
-    selectcoursetime = db.Column(db.String(20), nullable=False)
+    institution = db.Column(db.String(20), nullable=False)
+    qualification = db.Column(db.String(20), nullable=False)
+    grade = db.Column(db.String(20), nullable=False)
+    state = db.Column(db.String(20), nullable=False)
+    lga = db.Column(db.String(20), nullable=False)
+    trcn = db.Column(db.String(20), nullable=False)
+    courseofstudy = db.Column(db.String(20), nullable=False)
     address = db.Column(db.String(100), nullable=False)
-    line = db.Column(db.String(100), nullable=False)
     city = db.Column(db.String(100), nullable=False)
     zipcode = db.Column(db.String(20), nullable=False)
 
@@ -73,15 +79,21 @@ class Form(db.Model):
 
     user = db.relationship('User', backref='form_user', lazy=True)
 
-    def __init__(self, name, birthday, gender, phonenumber, selectcourse, selectcoursetime, address, line, city, zipcode):
+    def __init__(self, photo_path, name, birthday, gender, marital, phonenumber, institution, qualification, grade, state, lga, trcn, courseofstudy, address, city, zipcode):
+        self.photo_path = photo_path
         self.name = name
         self.birthday = birthday
         self.gender = gender
+        self.marital = marital
         self.phonenumber = phonenumber
-        self.selectcourse = selectcourse
-        self.selectcoursetime = selectcoursetime
+        self.institution = institution
+        self.qualification = qualification
+        self.grade = grade
+        self.state = state
+        self.lga = lga
+        self.trcn = trcn
+        self.courseofstudy = courseofstudy
         self.address = address
-        self.line = line
         self.city = city
         self.zipcode = zipcode
 
@@ -98,24 +110,6 @@ class Material(db.Model):
         self.description = description
         self.file_path = file_path
         self.form_id = form_id
-
-
-class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    content = db.Column(db.Text)
-    author = db.Column(db.String(255))
-    slug = db.Column(db.String(255))
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    # user = db.relationship('User', backref='blog_user', lazy=True)
-
-    def __init__(self, title, content, author, slug):
-        self.title = title
-        self.content = content
-        self.author = author
-        self.slug = slug
-        # self.user_id = user_id
 
 
 class RegisterForm(FlaskForm):
@@ -169,41 +163,46 @@ class LoginForm(FlaskForm):
                 "Incorrect password. Please check your password.")
 
 
-class BlogForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
-    content = StringField("Content", validators=[
-                          DataRequired()], widget=TextArea())
-    author = StringField("Author", validators=[DataRequired()])
-    slug = StringField("Slug", validators=[DataRequired()])
-
-    submit = SubmitField("Submit")
-
-
 class RegForm(FlaskForm):
+    photo_path = FileField('Upload Photo', validators=[
+        FileAllowed(['jpg', 'png', 'jpeg'])])
     name = StringField(validators=[InputRequired(), Length(
         min=4, max=100)], render_kw={"placeholder": "Full Name"})
     birthday = DateTimeField(validators=[InputRequired(
     )], format='%Y-%m-%d', render_kw={"placeholder": "Birth Day"})
     gender = SelectField(validators=[InputRequired()], choices=[('select', 'Select'), (
         'female', 'Female'), ('male', 'Male'), ('other', 'Other')], render_kw={"placeholder": "Gender"})
+    marital = SelectField(validators=[InputRequired()], choices=[('select', 'Marital Status'), (
+        'married', 'Married'), ('single', 'Single'), ('divorced', 'Divorced')], render_kw={"placeholder": "Marital Status"})
     phonenumber = IntegerField(validators=[InputRequired()], render_kw={
                                "placeholder": "Phone Number"})
-    selectcourse = SelectField(validators=[InputRequired()], choices=[('select choice', 'Select Choice'), ('language', 'Language'), ('communication', 'Communication'), ('business', 'Business'), ('software', 'Software'), ('social media', 'Social Media'), ('photography', 'Photography'), (
-        'web designing', 'Web Designing'), ('web technology', 'Web Technology'), ('pc systems', 'PC Systems'), ('it foundations', 'IT Foundations'), ('hr management', 'HR Management'), ('modeling', 'Modeling'), ('basic marketing', 'Basic Marketing')], render_kw={"placeholder": "Select Course"})
-    selectcoursetime = SelectField(validators=[InputRequired()], choices=[('select time', 'Select Time'), ('hours: 8am - 10am', 'Hours: 8am - 10am'), ('hours: 10am - 12pm', 'Hours: 10am - 12pm'),
-                                   ('hours: 12pm - 4pm', 'Hours: 12pm - 4pm'), ('hours: 4pm - 7pm', 'Hours: 4pm - 7pm'), ('hours: 7pm - 9pm', 'Hours: 7pm - 9pm')], render_kw={"placeholder": "Select Course Time"})
+    institution = StringField(validators=[InputRequired(), Length(
+        min=4, max=100)], render_kw={"placeholder": "Institution"})
+    qualification = SelectField(validators=[InputRequired()], choices=[('select qualification', 'Select Qualification'), ('bachelors degree', 'Bachelors Degree'), ('hnd/nd', 'HND/ND'),
+                                                                       ('nce', 'NCE'), ('others', 'Others')], render_kw={"placeholder": "Highest Qualification"})
+
+    grade = SelectField(validators=[InputRequired()], choices=[('select grade', 'Select Grade'), ('first class', 'First Class'), ('second class upper', 'Second Class Upper'),
+                                                               ('second class lower', 'Second Class Lower'), ('third class', 'Third Class'), ('pass', 'Pass'), ('others', 'Others')], render_kw={"placeholder": "Grade"})
+    state = SelectField(validators=[InputRequired()], choices=[('select state', 'Select State'), ('ondo state', 'Ondo State'), ('ogun state', 'Ogun state'),
+                                                               ('osun state', 'Osun State'), ('oyo state', 'Oyo State'), ('ekiti state', 'Ekiti State'), ('others', 'Others')], render_kw={"placeholder": "State of Origin"})
+    lga = StringField(validators=[InputRequired(), Length(
+        min=4, max=100)], render_kw={"placeholder": "Local Govt"})
+    trcn = SelectField(validators=[InputRequired()], choices=[('select', 'Are you a Registered Member of TRCN'), (
+        'yes', 'Yes'), ('no', 'No')], render_kw={"placeholder": "Are you a Registered Member of TRCN"})
+
+    courseofstudy = StringField(validators=[InputRequired(), Length(
+        min=4, max=100)], render_kw={"placeholder": "Course of Study"})
     address = StringField(validators=[InputRequired(), Length(
         min=7, max=100)], render_kw={"placeholder": "Address"})
-    line = StringField(validators=[InputRequired(), Length(
-        min=7, max=100)], render_kw={"placeholder": "Line"})
     city = StringField(validators=[InputRequired(), Length(
         min=7, max=100)], render_kw={"placeholder": "City"})
     zipcode = StringField(validators=[InputRequired(), Length(
         min=4, max=100)], render_kw={"placeholder": "Zip Code"})
+
     material_name = StringField(validators=[Length(max=100)], render_kw={
-                                "placeholder": "Material Name"})
+        "placeholder": "Material Name"})
     material_description = StringField(validators=[Length(max=255)], render_kw={
-                                       "placeholder": "Material Description"})
+        "placeholder": "Material Description"})
     material_file = FileField('Upload Material')
     submit = SubmitField("Submit")
 
@@ -300,103 +299,95 @@ def register():
 def form():
     form = RegForm()
 
+    # Check if the user has already submitted a form
+    existing_form = Form.query.filter_by(user_id=current_user.id).first()
+
+    if existing_form:
+        flash("You have already submitted a form!")
+        return redirect(url_for('dashboard'))
+
     if form.validate_on_submit():
         # Get the current user who is logged in
         user = current_user
 
         if not validate_user_info(user.id, form.name.data, form.phonenumber.data):
             flash(
-                'Name do not match with your profile creation. Please check your information.')
-
+                'Name does not match your profile creation. Please check your information.')
             return render_template("form.html", form=form)
 
-        # Check if the user is logged in and has a valid user ID
-        if user and user.id:
-            new_form = Form(
-                name=form.name.data,
-                birthday=form.birthday.data,
-                gender=form.gender.data,
-                phonenumber=form.phonenumber.data,
-                selectcourse=form.selectcourse.data,
-                selectcoursetime=form.selectcoursetime.data,
-                address=form.address.data,
-                line=form.line.data,
-                city=form.city.data,
-                zipcode=form.zipcode.data,
+        photo_path = None
+
+        # Handle photo upload
+        if form.photo_path.data:  # Check if a photo has been uploaded
+            # Save the uploaded photo to a directory (you can customize this)
+            photo_path = os.path.join(
+                app.config['UPLOAD_FOLDER'], secure_filename(form.photo_path.data.filename))
+            form.photo_path.data.save(photo_path)
+
+        # Create a new form record
+        new_form = Form(
+            photo_path=photo_path,  # Save the photo file path in the database
+            name=form.name.data,
+            birthday=form.birthday.data,
+            gender=form.gender.data,
+            marital=form.marital.data,
+            phonenumber=form.phonenumber.data,
+            institution=form.institution.data,
+            qualification=form.qualification.data,
+            grade=form.grade.data,
+            state=form.state.data,
+            lga=form.lga.data,
+            trcn=form.trcn.data,
+            courseofstudy=form.courseofstudy.data,
+            address=form.address.data,
+            city=form.city.data,
+            zipcode=form.zipcode.data
+        )
+
+        new_form.user = current_user
+
+        db.session.add(new_form)
+        db.session.commit()
+
+        # Handle material upload only if the form submission is valid
+        if form.material_file.data:
+            # Save the uploaded file to a directory (you can customize this)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(
+                form.material_file.data.filename))
+            form.material_file.data.save(file_path)
+
+            # Create a new material associated with the form submission
+            new_material = Material(
+                name=form.material_name.data,
+                description=form.material_description.data,
+                file_path=file_path,
+                form_id=new_form.id
             )
 
-            new_form.user = current_user
-
-            db.session.add(new_form)
+            db.session.add(new_material)
             db.session.commit()
 
-            # Handle material upload only if the form submission is valid
-            if form.material_file.data:
-                # Save the uploaded file to a directory (you can customize this)
-                file_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(
-                    form.material_file.data.filename))
-                form.material_file.data.save(file_path)
-
-                # Create a new material associated with the form submission
-                new_material = Material(
-                    name=form.material_name.data,
-                    description=form.material_description.data,
-                    file_path=file_path,
-                    form_id=new_form.id
-                )
-
-                db.session.add(new_material)
-                db.session.commit()
-            return redirect(url_for('dashboard'))
+        return redirect(url_for('dashboard'))
 
     return render_template("form.html", form=form)
 
-# Edit Form
+
+# print form
 
 
-@app.route("/edit_form/<int:form_id>", methods=['GET', 'POST'])
+@app.route("/print_form/<int:form_id>")
 @login_required
-def edit_form(form_id):
-    form = RegForm()
-    edited_form = Form.query.filter_by(
+def print_form(form_id):
+    user_form = Form.query.filter_by(
         id=form_id, user_id=current_user.id).first()
-
-    if edited_form is None:
+    if user_form:
+        return render_template("print_form.html", user_form=user_form)
+    else:
+        flash("Form not found.")
         return redirect(url_for('dashboard'))
 
-    if form.validate_on_submit():
-        # Update the form record with the edited data
-        edited_form.name = form.name.data
-        edited_form.birthday = form.birthday.data
-        edited_form.gender = form.gender.data
-        edited_form.phonenumber = form.phonenumber.data
-        edited_form.selectcourse = form.selectcourse.data
-        edited_form.selectcoursetime = form.selectcoursetime.data
-        edited_form.address = form.address.data
-        edited_form.line = form.line.data
-        edited_form.city = form.city.data
-        edited_form.zipcode = form.zipcode.data
-
-        db.session.commit()
-        return redirect(url_for('dashboard'))
-
-    # Populate the form fields with existing data
-    form.name.data = edited_form.name
-    form.birthday.data = edited_form.birthday
-    form.gender.data = edited_form.gender
-    form.phonenumber.data = edited_form.phonenumber
-    form.selectcourse.data = edited_form.selectcourse
-    form.selectcoursetime.data = edited_form.selectcoursetime
-    form.address.data = edited_form.address
-    form.line.data = edited_form.line
-    form.city.data = edited_form.city
-    form.zipcode.data = edited_form.zipcode
-
-    return render_template("edit_form.html", form=form, user=current_user)
 
 # Delete Form
-
-
 @app.route("/delete_form/<int:form_id>", methods=['POST'])
 @login_required
 def delete_form(form_id):
@@ -443,66 +434,15 @@ def delete_material(material_id):
     return redirect(url_for('dashboard'))
 
 
+@app.route("/teachform.html")
+@login_required
+def teach():
+    return render_template("teachform.html")
+
+
 @app.route("/about.html")
 def about():
     return render_template("about.html")
-
-
-@app.route("/add_blog.html", methods=['GET', 'POST'])
-@login_required
-def addblog():
-    form = BlogForm()
-
-    if form.validate_on_submit():
-        post = Blog(
-            title=form.title.data,
-            content=form.content.data,
-            author=form.author.data,
-            slug=form.slug.data,
-            # user_id=current_user.id
-        )
-
-        post.date_posted = datetime.utcnow()
-
-        form.title.data = ''
-        form.content.data = ''
-        form.author.data = ''
-        form.slug.data = ''
-        # form.user_id.data = ''
-
-        db.session.add(post)
-        db.session.commit()
-
-        flash("Blog Post successful")
-
-    return render_template("add_blog.html", form=form)
-
-
-@app.route("/blog.html")
-@login_required
-def blog():
-    # posts = Blog.query.order_by(Blog.author)
-    # return render_template("blog.html", posts=posts)
-
-    user = current_user
-    posts = Blog.query.order_by(Blog.author)
-
-    return render_template("blog.html", posts=posts, user=current_user)
-
-
-@app.route("/business.html")
-def business():
-    return render_template("business.html")
-
-
-@app.route("/coming_soon.html")
-def coming_soon():
-    return render_template("coming_soon.html")
-
-
-@app.route("/communication.html")
-def communication():
-    return render_template("communication.html")
 
 
 @app.route("/contact.html")
@@ -510,49 +450,9 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/course_details.html")
-def course_details():
-    return render_template("course_details.html")
-
-
 @app.route("/faq.html")
 def faq():
     return render_template("faq.html")
-
-
-@app.route("/gallery.html")
-def gallery():
-    return render_template("gallery.html")
-
-
-@app.route("/language.html")
-def language():
-    return render_template("language.html")
-
-
-@app.route("/photography.html")
-def photography():
-    return render_template("photography.html")
-
-
-@app.route("/single.html")
-def single():
-    return render_template("single.html")
-
-
-@app.route("/social_media.html")
-def social_media():
-    return render_template("social_media.html")
-
-
-@app.route("/software.html")
-def software():
-    return render_template("software.html")
-
-
-@app.route("/404.html")
-def err404():
-    return render_template("404.html")
 
 
 if __name__ == '__main__':
